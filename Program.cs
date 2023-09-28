6f83ed747a380c034d9cd6ed1fee006990c8272e
wesleyruchel <wesleyruchel@gmail.com>
2023-09-28 20:13:17 -0300

    Handle multiple connections

--- Program.cs
+++ Program.cs
@@ -3,9 +3,20 @@ using System.Text;
 
 var webSocket = new ClientWebSocket();
 
-Console.WriteLine("Connection to SERVER");
+string username;
 
-await webSocket.ConnectAsync(new Uri("ws://localhost:5001/"), CancellationToken.None);
+while (true)
+{
+    Console.Write("Input username:");
+
+    username = Console.ReadLine();
+
+    break;
+}
+
+Console.WriteLine("Connecting to SERVER");
+
+await webSocket.ConnectAsync(new Uri($"ws://localhost:5001/?username={username}"), CancellationToken.None);
 
 Console.WriteLine("Connected!");
 
@@ -15,14 +26,41 @@ var receiveTask = Task.Run(async () =>
     while (true)
     {
         var receive = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-        
+
         if (receive.MessageType == WebSocketMessageType.Close)
+        {
             break;
+        }
 
         var data = Encoding.UTF8.GetString(buffer, 0, receive.Count);
 
-        Console.WriteLine("Received: " + data);
+        Console.WriteLine(data);
     }
 });
 
-await receiveTask;
+var sendTask = Task.Run(async () =>
+{
+    while (true)
+    {
+        var message = Console.ReadLine();
+
+        if (message == "exit")
+        {
+            break;
+        }
+
+        var bytes = Encoding.UTF8.GetBytes(message);
+
+        await webSocket.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, CancellationToken.None);
+    }
+});
+
+
+await Task.WhenAny(sendTask, receiveTask);
+
+if (webSocket.State != WebSocketState.Closed)
+{
+    await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
+}
+
+await Task.WhenAll(sendTask, receiveTask);
