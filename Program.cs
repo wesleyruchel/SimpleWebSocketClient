069acb12bac26c58e68dc3c6b8e2975ff46650b7
wesleyruchel <wesleyruchel@gmail.com>
2023-09-28 11:39:22 -0300

    Implementing a simple client

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.WebSockets;
+using System.Text;
+
+var webSocket = new ClientWebSocket();
+
+Console.WriteLine("Connection to SERVER");
+
+await webSocket.ConnectAsync(new Uri("ws://localhost:5001/"), CancellationToken.None);
+
+Console.WriteLine("Connected!");
+
+var receiveTask = Task.Run(async () =>
+{
+    var buffer = new byte[1024];
+    while (true)
+    {
+        var receive = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+        
+        if (receive.MessageType == WebSocketMessageType.Close)
+            break;
+
+        var data = Encoding.UTF8.GetString(buffer, 0, receive.Count);
+
+        Console.WriteLine("Received: " + data);
+    }
+});
+
+await receiveTask;
